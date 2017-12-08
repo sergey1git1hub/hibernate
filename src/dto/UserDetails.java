7a0b3f3f499fbcc4c779e5209dcec9dff35af1de
schubuk <nameserhiy@gmail.com>
2017-12-08 10:52:25 +0200

    End of lesson 5.

--- src/dto/UserDetails.java
+++ src/dto/UserDetails.java
@@ -1,35 +1,73 @@
 package dto;
 
+import java.util.Date;
+
+import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 
-@Entity(name = "USER_DETAILS")
+@Entity
+@Table(name = "")
 public class UserDetails {
 
+	@Id
 	int userId;
 	
-	@Id
-	@Column(name = "USER_ID")
+	@Transient
+	String userName;
+	public Date getJoinedDate() {
+		return joinedDate;
+	}
+
+	public void setJoinedDate(Date joinedDate) {
+		this.joinedDate = joinedDate;
+	}
+
+	public String getAddress() {
+		return Address;
+	}
+
+	public void setAddress(String address) {
+		Address = address;
+	}
+
+	public String getDescription() {
+		return Description;
+	}
+
+	public void setDescription(String description) {
+		Description = description;
+	}
+
+	@Temporal(TemporalType.DATE)
+	private Date joinedDate;
+	private String Address;
+	
+	@Lob
+	private String Description;
+	
 	public int getUserId() {
 		return userId;
 	}
-	
 
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
 	
 	
-	@Column(name = "USER_NAME")
 	public String getUserName() {
 		return userName + " from getter.";
 	}
-	
 	
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
 	
-	String userName;
+	
 }
--- src/hibernate/HibernateTest.java
+++ src/hibernate/HibernateTest.java
@@ -1,5 +1,7 @@
 package hibernate;
 
+import java.util.Date;
+
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.classic.Session;
@@ -11,9 +13,11 @@ public class HibernateTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		UserDetails user = new UserDetails();
-		user.setUserId(3);
-		user.setUserName("Third User");
-		
+		user.setUserId(1);
+		user.setUserName("First User");
+		user.setAddress("First user address.");
+		user.setDescription("First user description.");
+		user.setJoinedDate(new Date());
 	
 		
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
